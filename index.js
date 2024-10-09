c954438c3de91533911d85eb34c2e6c230eab8bf
Pawan Patidar <121277602+Starkds@users.noreply.github.com>
2024-10-09 19:18:50 +0000

    style added to the element

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const URL = require('./models/url.models.js');
 const app = express();
 
 
-const  port = process.env.PORT  || 5000;
+const  port = process.env.PORT  || 5000 || 6000 || 7000;
 const dbURL = process.env.MONGOURI;
 
 
